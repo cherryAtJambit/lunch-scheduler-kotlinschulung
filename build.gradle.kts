f22134f8c5b7cfe64fb062bd6201fb00614586bd
Rene Preissel <rp@etosquare.de>
2020-06-22 17:46:37 +0200

    Kotest new version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val kotlinCoroutineVersion by extra("1.3.5")
 val ktorVersion: String by extra { "1.3.2" }
 val junitVersion: String by extra { "5.6.2" }
-val kotestVersion: String by extra { "4.0.5" }
+val kotestVersion: String by extra { "4.1.0.RC2" }
 val log4jVersion: String by extra { "2.13.1" }
 val jsonpathVersion: String by extra { "2.4.0" }
 val mockkVersion: String by extra { "1.10.0" }
