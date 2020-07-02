cd6227d3278842b9bb18498ee2fbd313ba0de2a0
Sebastian Ingenhütt <sebastian.ingenhuett@jambit.com>
2020-07-02 11:12:22 +0200

    Add controller template and basic auth

--- lunch-scheduler/src/main/kotlin/LunchSchedulerMain.kt
+++ lunch-scheduler/src/main/kotlin/LunchSchedulerMain.kt
@@ -1,24 +1,12 @@
 package de.e2.lunch_scheduler
 
-import io.ktor.application.Application
-import io.ktor.application.call
-import io.ktor.response.respondText
-import io.ktor.routing.get
-import io.ktor.routing.routing
+import de.e2.lunch_scheduler.com.jambit.controller.friendsController
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import org.koin.ktor.ext.inject
-import org.koin.ktor.ext.modules
-
-fun Application.lunchScheduler() {
-    routing {
-        get("/") {
-            call.respondText("TODO")
-        }
-    }
-}
 
 fun main() {
-    val server = embeddedServer(factory = Netty, port = 8080, module = Application::lunchScheduler)
+    val server = embeddedServer(factory = Netty, port = 8080) {
+        friendsController()
+    }
     server.start(wait = true)
 }
---
+++ lunch-scheduler/src/main/kotlin/com/jambit/controller/FriendsController.kt
@@ -0,0 +1,47 @@
+package de.e2.lunch_scheduler.com.jambit.controller
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.auth.UserIdPrincipal
+import io.ktor.auth.authenticate
+import io.ktor.auth.basic
+import io.ktor.features.ContentNegotiation
+import io.ktor.jackson.jackson
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.routing
+
+const val AUTH_NAME = "basic" //TOPIC const
+
+fun Application.friendsController() {
+    install(ContentNegotiation) {
+        jackson()
+    }
+
+    install(Authentication) {
+        basic(name = AUTH_NAME) {
+            realm = "Ktor Server"
+            validate { credentials ->
+                if (credentials.name == credentials.password) {
+                    UserIdPrincipal(credentials.name)
+                } else {
+                    null
+                }
+            }
+        }
+    }
+
+    routing {
+        authenticate(AUTH_NAME) {
+            get("/api/friends") {
+                call.respondText("Hello")
+            }
+            post("/api/friends") {
+                // TODO
+            }
+        }
+    }
+}
