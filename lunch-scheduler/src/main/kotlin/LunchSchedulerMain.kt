b9eca3deb63d8c50df05b3ad9f622a3d79d9e7aa
Michael Kirsch <michael.kirsch@jambit.com>
2020-07-02 13:51:58 +0200

    import test data

--- lunch-scheduler/src/main/kotlin/LunchSchedulerMain.kt
+++ lunch-scheduler/src/main/kotlin/LunchSchedulerMain.kt
@@ -1,8 +1,13 @@
 package de.e2.lunch_scheduler
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import de.e2.lunch_scheduler.com.jambit.controller.AUTH_NAME
 import de.e2.lunch_scheduler.com.jambit.controller.friendsController
 import de.e2.lunch_scheduler.com.jambit.controller.meController
+import de.e2.lunch_scheduler.de.e2.lunch_scheduler.com.jambit.model.RtService
+import de.e2.lunch_scheduler.de.e2.lunch_scheduler.com.jambit.model.User
 import io.ktor.application.install
 import io.ktor.auth.Authentication
 import io.ktor.auth.UserIdPrincipal
@@ -11,6 +16,7 @@ import io.ktor.features.ContentNegotiation
 import io.ktor.jackson.jackson
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import java.io.File
 
 fun main() {
     val server = embeddedServer(factory = Netty, port = 8080) {
@@ -31,9 +37,23 @@ fun main() {
                 }
             }
         }
-
+        init()
         friendsController()
         meController()
     }
     server.start(wait = true)
 }
+
+fun init() {
+    val mapper = jacksonObjectMapper()
+    mapper.registerKotlinModule()
+
+    val jsonString: String = File("./lunch-scheduler/src/main/kotlin/com.jambit.model/Users.json").readText(Charsets.UTF_8)
+    val jsonTextList: List<UsersToImport> = mapper.readValue(jsonString)
+    for (user in jsonTextList) {
+
+        RtService.saveUser(User(null, user.Name))
+    }
+}
+
+data class UsersToImport(val Name: String, val PhotoUrl: String, val AuthorizationTokenExpiration: String, val AuthenticationProviderKind: Int, val AuthenticationProviderId: String)
