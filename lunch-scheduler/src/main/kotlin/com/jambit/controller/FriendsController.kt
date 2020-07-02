e7c4dcb0f4badd37caf1b063f5adbc6c408653df
Sebastian Ingenhütt <sebastian.ingenhuett@jambit.com>
2020-07-02 11:41:00 +0200

    Add endpoints for FriendsController

--- lunch-scheduler/src/main/kotlin/com/jambit/controller/FriendsController.kt
+++ lunch-scheduler/src/main/kotlin/com/jambit/controller/FriendsController.kt
@@ -2,13 +2,9 @@ package de.e2.lunch_scheduler.com.jambit.controller
 
 import io.ktor.application.Application
 import io.ktor.application.call
-import io.ktor.application.install
-import io.ktor.auth.Authentication
-import io.ktor.auth.UserIdPrincipal
 import io.ktor.auth.authenticate
-import io.ktor.auth.basic
-import io.ktor.features.ContentNegotiation
-import io.ktor.jackson.jackson
+import io.ktor.request.receive
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.post
@@ -18,15 +14,30 @@ const val AUTH_NAME = "basic" //TOPIC const
 
 fun Application.friendsController() {
 
-
     routing {
         authenticate(AUTH_NAME) {
+
             get("/api/friends") {
-                call.respondText("Hello")
+                call.respondText("Friend controller is running")
             }
+
             post("/api/friends") {
-                // TODO
+                val newNewFriendshipRequest: NewFriendshipRequest = call.receive()
+                friendsRepository.add(newNewFriendshipRequest.newFriendship)
+                call.respond(NewFriendshipResponse("Friendship established."))
+            }
+
+            get("/api/suggest") {
+                val user: User = meRepository.getMe()
+                val suggestedFriends: List<User> = friendsRepository.getAllUnFriends(user.userId)
+                call.respond(SuggestedFriendsResponse(suggestedFriends))
             }
         }
     }
 }
+
+data class SuggestedFriendsResponse(val suggestedFriends: List<User>)
+
+data class NewFriendshipRequest(val newFriendship: Friendship)
+
+data class NewFriendshipResponse(val result: String)
