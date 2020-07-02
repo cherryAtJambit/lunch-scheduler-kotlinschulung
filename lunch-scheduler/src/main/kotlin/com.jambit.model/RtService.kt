c3a877c1a0279a7c94443f8139e95102a2d6373e
Martin Weckwerth <martin.weckwerth@jambit.com>
2020-07-02 13:44:45 +0200

    some documentation

--- lunch-scheduler/src/main/kotlin/com.jambit.model/RtService.kt
+++ lunch-scheduler/src/main/kotlin/com.jambit.model/RtService.kt
@@ -7,12 +7,14 @@ object RtService {
     private val users = mutableMapOf<String, User>()
     private val friendships = mutableMapOf<String, Friendship>()
 
+    // creates or updates a user; returns old user if any
     fun saveUser(user: User): User? {
         val id: String = user.id ?: UUID.randomUUID().toString();
         val newUser: User = if (user.id == null) User(id, user.name) else user
         return users.put(id, newUser)
     }
 
+    // creates or updates a friendship; returns old friendship if any
     fun saveFriendship(friendship: Friendship): Friendship? {
         val id: String = friendship.id ?: UUID.randomUUID().toString();
         val newUser: Friendship = if (friendship.id == null) Friendship(id, friendship.userId, friendship.friendUserId) else friendship
@@ -27,10 +29,10 @@ object RtService {
 
     fun findFirstUserByName(name: String): User? = users.values.asSequence().firstOrNull { it.name == name }
 
-    fun findEnemiesByUserId(userId:String): Set<User> {
-        val allUserIds:Set<String> = users.values.asSequence().mapNotNull { it.id } .toSet()
-        val friendIds:Set<String> = friendships.values.asSequence().filter { it.userId==userId }.map { it.friendUserId }.toSet()
-        val enemyIds:Set<String> = allUserIds-friendIds;
+    fun findEnemiesByUserId(userId: String): Set<User> {
+        val allUserIds: Set<String> = users.values.asSequence().mapNotNull { it.id }.toSet()
+        val friendIds: Set<String> = friendships.values.asSequence().filter { it.userId == userId }.map { it.friendUserId }.toSet()
+        val enemyIds: Set<String> = allUserIds - friendIds;
         return enemyIds.asSequence().mapNotNull { users[it] }.toSet()
     }
 }
