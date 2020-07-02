ce4253dd1a63147a531ea59f7af2bc4dc28aa0b8
Sebastian Ingenhütt <sebastian.ingenhuett@jambit.com>
2020-07-02 13:40:43 +0200

    Add friends endpoint to MeController, return JSON on me endpoint

--- lunch-scheduler/src/main/kotlin/com/jambit/controller/MeController.kt
+++ lunch-scheduler/src/main/kotlin/com/jambit/controller/MeController.kt
@@ -1,14 +1,14 @@
 package de.e2.lunch_scheduler.com.jambit.controller
 
+import de.e2.lunch_scheduler.de.e2.lunch_scheduler.com.jambit.model.RtService
+import de.e2.lunch_scheduler.de.e2.lunch_scheduler.com.jambit.model.User
 import io.ktor.application.Application
 import io.ktor.application.call
-import io.ktor.application.install
-import io.ktor.auth.*
-import io.ktor.features.ContentNegotiation
-import io.ktor.jackson.jackson
-import io.ktor.response.respondText
+import io.ktor.auth.UserIdPrincipal
+import io.ktor.auth.authenticate
+import io.ktor.auth.principal
+import io.ktor.response.respond
 import io.ktor.routing.get
-import io.ktor.routing.post
 import io.ktor.routing.routing
 
 
@@ -17,9 +17,25 @@ fun Application.meController() {
     routing {
         authenticate(AUTH_NAME) {
             get("/api/me") {
-                val principal = call.principal<UserIdPrincipal>()
-                call.respondText("Authorised with user: ${principal?.name}")
+                val userName = call.principal<UserIdPrincipal>()?.name ?: throw Exception("No username specified.")
+                val user = RtService.findFirstUserByName(userName)
+                        ?: throw Exception("No user found with name '$userName'.")
+
+                call.respond(MeResponse(user))
+            }
+
+            get("/api/me/friends") {
+                val userName = call.principal<UserIdPrincipal>()?.name ?: throw Exception("No username specified.")
+                val user = RtService.findFirstUserByName(userName)
+                        ?: throw Exception("No user found with name '$userName'.")
+
+                val friends: List<User> = RtService.findFriendsByUserId(user.id)
+                call.respond(FriendsResponse(friends))
             }
         }
     }
 }
+
+data class FriendsResponse(val friends: List<User>)
+
+data class MeResponse(val me: User)
