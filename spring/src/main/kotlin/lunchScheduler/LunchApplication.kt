6d6bbbb33a66cc84ad0db50302e624bc7f1f878f
Michael Kirsch <michael.kirsch@jambit.com>
2020-07-03 14:14:10 +0200

    implement spring security

--- spring/src/main/kotlin/lunchScheduler/LunchApplication.kt
+++ spring/src/main/kotlin/lunchScheduler/LunchApplication.kt
@@ -1,15 +1,22 @@
 package de.e2.spring.lunchScheduler
 
-import de.e2.spring.annotations.AdresseAnnotationApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.support.beans
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
-import org.springframework.http.ResponseEntity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.web.servlet.invoke
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.function.ServerResponse
+import org.springframework.web.servlet.function.router
 
-data class User(val name: String, val id: String? = null)
+data class LunchUser(val name: String, val id: String? = null)
 
 @SpringBootApplication
 class LunchApplication
@@ -19,14 +26,45 @@ class LunchApplication
 class LunchController() {
 
     @GetMapping("/me", produces = arrayOf(APPLICATION_JSON_VALUE))
-    fun getMe(): User {
+    fun getMe(): LunchUser {
 
-        val user = User("Cherry")
+        val user = LunchUser("Cherry")
 
         return user
     }
 }
 
+@EnableWebSecurity
+class KotlinSecurityConfiguration : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity?) {
+        http {
+            httpBasic {}
+            authorizeRequests {
+                authorize("/greetings/**", hasAuthority("ROLE_ADMIN"))
+                authorize("/**", permitAll)
+            }
+        }
+    }
+}
+
 fun main(args: Array<String>) {
-    runApplication<LunchApplication>(*args)
+    runApplication<LunchApplication>(*args) {
+        addInitializers(beans {
+            bean {
+
+                fun user(user: String, pw: String, vararg roles: String) =
+                        User.withDefaultPasswordEncoder().username(user).password(pw).roles(*roles).build()
+
+                InMemoryUserDetailsManager(user("admin", "admin", "USER"))
+            }
+            bean {
+                router {
+                    GET("/greetings") { request ->
+                        request.principal().map { it.name }.map { ServerResponse.ok().body(mapOf("greeting" to "Hello, $it")) }.orElseGet { ServerResponse.badRequest().build() }
+                    }
+                }
+            }
+        })
+    }
 }
