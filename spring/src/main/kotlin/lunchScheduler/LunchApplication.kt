36efb17898ff2ab4d63dc120fe655f454b765a81
Martin Weckwerth <martin.weckwerth@jambit.com>
2020-07-03 14:13:26 +0200

    removes unused imports

--- spring/src/main/kotlin/lunchScheduler/LunchApplication.kt
+++ spring/src/main/kotlin/lunchScheduler/LunchApplication.kt
@@ -1,10 +1,8 @@
 package de.e2.spring.lunchScheduler
 
-import de.e2.spring.annotations.AdresseAnnotationApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
